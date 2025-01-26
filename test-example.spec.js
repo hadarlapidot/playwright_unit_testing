ed75efbeff9e51e45c5960074681c41f34e1219f
hadarlapidot <hadarlapidot@gmail.com>
2025-01-26 13:06:08 +0200

    Update test-example.spec.js

--- test-example.spec.js
+++ test-example.spec.js
@@ -3,7 +3,7 @@ const { chromium } = require("playwright");
 // IIFE
 (async () => {
   // Launch the browser
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
